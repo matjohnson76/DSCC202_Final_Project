2c8d1e21c009150ace59b1b190e5ca9964193fbe
calebtaber@gmail.com <calebtaber@gmail.com>
2022-05-04 01:41:41 +0000

    Data Flows!

--- 01 ETL.py
+++ 01 ETL.py
@@ -54,32 +54,6 @@ print("Tokens assertion passed")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED. SEE PYSPARK BELOW
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC DROP TABLE IF EXISTS tokens_silver;
-# MAGIC 
-# MAGIC CREATE TABLE tokens_silver
-# MAGIC (
-# MAGIC   address STRING,
-# MAGIC   name STRING,
-# MAGIC   symbol STRING,
-# MAGIC   price_usd DOUBLE
-# MAGIC )
-# MAGIC USING delta;
-# MAGIC 
-# MAGIC -- TODO add index int that can be used in token_transfers_silver to reduce the size?
-# MAGIC 
-# MAGIC INSERT INTO tokens_silver
-# MAGIC   SELECT DISTINCT TPU.contract_address, TPU.name, TPU.symbol, TPU.price_usd
-# MAGIC   FROM ethereumetl.token_prices_usd TPU INNER JOIN ethereumetl.tokens T ON contract_address=address
-# MAGIC     WHERE asset_platform_id = 'ethereum';
-
-# COMMAND ----------
-
 # CALEB
 # Strips down the tokens table to only ERC20 tokens. Also, adds pricing information
 # Only tracks tokens included in the token_prices_usd table since tokens without pricing info are not of interest to us
@@ -125,32 +99,6 @@ print("tokens_silver_sub assertion passed")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED. SEE PYSPARK BELOW
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC DROP TABLE IF EXISTS token_transfers_silver;
-# MAGIC 
-# MAGIC CREATE TABLE token_transfers_silver
-# MAGIC (
-# MAGIC   token_address STRING,
-# MAGIC   from_address STRING,
-# MAGIC   to_address STRING,
-# MAGIC   value DECIMAL(38,0),
-# MAGIC   timestamp TIMESTAMP
-# MAGIC )
-# MAGIC USING delta;
-# MAGIC 
-# MAGIC 
-# MAGIC INSERT INTO token_transfers_silver
-# MAGIC   SELECT T.address, TT.from_address, TT.to_address, TT.value, CAST(B.timestamp AS TIMESTAMP)
-# MAGIC   FROM tokens_silver T, ethereumetl.token_transfers TT, ethereumetl.blocks B
-# MAGIC   WHERE T.address = TT.token_address AND TT.block_number = B.number;
-
-# COMMAND ----------
-
 # CALEB
 # Strips down the token_transfers table to a more manageable set of useful attributes
 # Also removes transfers that involve tokens not stored in the tokens_silver table (see above command)
@@ -179,51 +127,6 @@ tt_silver = (
 
 # COMMAND ----------
 
-# CALEB
-# Creates a table of unique wallet addresses and their corresponding numeric ID numbers
-
-from pyspark.sql.window import Window
-
-tt_silver = spark.table('g04_db.tt_silver')
-toks_silver = spark.table('g04_db.toks_silver')
-
-unique_addrs = tt_silver.select('to_address').distinct()\
-               .union(tt_silver.select('from_address').distinct())\
-               .distinct()\
-               .subtract(toks_silver.select('address'))\
-               .withColumn("addr_id", row_number().over(Window.orderBy(lit(1))))
-
-unique_addrs.write\
-            .format("delta")\
-            .mode("overwrite")\
-            .option('mergeSchema', True)\
-            .saveAsTable('g04_db.unique_wallets')
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED. SEE PYSPARK BELOW
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC DROP TABLE IF EXISTS etl_tok_trans_abridged;
-# MAGIC 
-# MAGIC CREATE TABLE etl_tok_trans_abridged(
-# MAGIC   token_address STRING,
-# MAGIC   from_address STRING,
-# MAGIC   to_address STRING,
-# MAGIC   value DECIMAL(38,0)
-# MAGIC )
-# MAGIC USING DELTA;
-# MAGIC 
-# MAGIC INSERT INTO etl_tok_trans_abridged
-# MAGIC   SELECT token_address, from_address, to_address, value
-# MAGIC   FROM token_transfers_silver
-# MAGIC   WHERE timestamp > CAST('${start.date}' AS TIMESTAMP);
-
-# COMMAND ----------
-
 tt_silver = spark.table('g04_db.tt_silver')
 
 assert tt_silver.schema == _parse_datatype_string("id: int, to_address: string, from_address: string, value: decimal(38,0), timestamp: timestamp"), "tt_silver schema is not validated"
@@ -239,7 +142,7 @@ tt_silver = spark.table('g04_db.tt_silver')
 
 tt_silver_abridged = (
                       tt_silver.select('*')
-                               .where(tt_silver.timestamp > start_date)
+                               .where(tt_silver.timestamp > '2021-01-01T00:00:00.000+000')
                      ).drop('timestamp')
 
 (tt_silver_abridged.write
@@ -249,146 +152,46 @@ tt_silver_abridged = (
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED
-# MAGIC -- Fills the tokens sold table for the specified wallet address
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC DROP TABLE IF EXISTS etl_toks_sold;
-# MAGIC 
-# MAGIC CREATE TABLE etl_toks_sold(
-# MAGIC   token_address STRING,
-# MAGIC   amt_sold DECIMAL(38,0)
-# MAGIC )
-# MAGIC USING DELTA;
-# MAGIC 
-# MAGIC INSERT INTO etl_toks_sold
-# MAGIC   SELECT token_address, SUM(value)
-# MAGIC   FROM etl_tok_trans_abridged
-# MAGIC   WHERE from_address = '${wallet.address}'
-# MAGIC   GROUP BY token_address;
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED
-# MAGIC -- Fills the tokens bought table for the specified wallet address
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC DROP TABLE IF EXISTS etl_toks_bought;
-# MAGIC 
-# MAGIC CREATE TABLE etl_toks_bought(
-# MAGIC   token_address STRING,
-# MAGIC   amt_bought DECIMAL(38,0)
-# MAGIC )
-# MAGIC USING DELTA;
-# MAGIC 
-# MAGIC INSERT INTO etl_toks_bought
-# MAGIC   SELECT token_address, SUM(value)
-# MAGIC   FROM etl_tok_trans_abridged
-# MAGIC   WHERE to_address = '${wallet.address}'
-# MAGIC   GROUP BY token_address;
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- DEPRACATED
-# MAGIC -- Displays the given wallet's token balance for the period
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC SELECT B.token_address AS Buy_Tok,
-# MAGIC        B.amt_bought, 
-# MAGIC        S.token_address AS Sell_Tok, 
-# MAGIC        S.amt_sold, 
-# MAGIC        (CASE WHEN S.amt_sold IS NULL THEN B.amt_bought 
-# MAGIC              WHEN B.amt_bought IS NULL THEN -S.amt_sold
-# MAGIC         ELSE B.amt_bought - S.amt_sold END) AS period_balance
-# MAGIC FROM etl_toks_bought B FULL OUTER JOIN etl_toks_sold S ON B.token_address = S.token_address;
-# MAGIC 
-# MAGIC -- Some balances may be negative because the tracking period does not necessarily start from the beginning of time
-# MAGIC -- For example, I set my start date to '2022-01-01'. When I run this code, there are usually a few tokens that come up with
-# MAGIC -- negative balances. This is almost certainly because the owner of the wallet bought some tokens of that type before 2022-01-01
-# MAGIC -- and only sold them afterwards.
-# MAGIC -- NOTE: I haven't yet tested this code with a start date corresponding to the first erc20 transaction
-# MAGIC 
-# MAGIC -- address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-# MAGIC -- Wrapped Ether (see addr above) is almost always negative
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC -- CALEB
-# MAGIC -- The results below are useful for testing the balance-checking code since the from_addresses are guranteed to be within the date range specified
-# MAGIC -- Just choose a from_address and input it to the widget
-# MAGIC 
-# MAGIC 
-# MAGIC USE g04_db;
-# MAGIC 
-# MAGIC SELECT from_address, timestamp
-# MAGIC FROM tt_silver_abridged
-# MAGIC ORDER BY timestamp DESC;
-
-# COMMAND ----------
-
 # CALEB
 
-unique_wallets = spark.table('g04_db.unique_wallets')
-
-triple = spark.createDataFrame(data=[], schema="wallet_id INT, tok_id INT, balance DOUBLE")
-
-triple.union()
-
-# COMMAND ----------
-
-# CALEB
-# Finds the period balance for a single wallet
-
-from pyspark.sql.functions import sum, when
-
+from pyspark.sql.functions import sum
 
 tt_silver_abridged = spark.table('g04_db.tt_silver_abridged')
+toks_silver = spark.table('g04_db.toks_silver')
 
-bought = (
-          tt_silver_abridged.select('*')
-                            .where(tt_silver_abridged.to_address == wallet_address)
-                            .groupBy('id')
-                            .agg(sum('value').alias('amt_bought'))
-                            .withColumnRenamed('id', 'b_id')
-         )
+bought = tt_silver_abridged.withColumnRenamed('to_address', 'to_addr')\
+                           .groupBy('id', 'to_addr')\
+                           .agg(sum('value').alias('b_val'))
 
+sold = tt_silver_abridged.withColumnRenamed('from_address', 'from_addr')\
+                         .groupBy('id', 'from_addr')\
+                         .agg(sum('value').alias('s_val'))
 
-sold = (
-          tt_silver_abridged.select('*')
-                            .where(tt_silver_abridged.from_address == wallet_address)
-                            .groupBy('id')
-                            .agg(sum('value').alias('amt_sold'))
-                            .withColumnRenamed('id', 's_id')
-       )
+with_bal = bought.join(sold, (bought.id == sold.id) & (bought.to_addr == sold.from_addr), 'inner')\
+                 .withColumn('balance', bought.b_val - sold.s_val)\
+                 .withColumnRenamed('to_addr', 'addr')\
+                 .select('addr', bought.id, 'balance')
 
+with_usd = with_bal.join(toks_silver, with_bal.id == toks_silver.id, 'inner')\
+                   .withColumn('bal_usd', with_bal.balance * toks_silver.price_usd)\
+                   .select(with_bal.addr, with_bal.id, 'bal_usd')
 
-balance = (
-           bought.join(sold, (bought.b_id == sold.s_id), 'fullouter')
-                 .withColumn('balance', when(bought.b_id.isNull(), -sold.amt_sold)
-                                       .when(sold.s_id.isNull(), bought.amt_bought)
-                                       .otherwise(bought.amt_bought - sold.amt_sold))
-          )
+addrs_abridged = with_usd.select('addr').distinct()\
+                         .withColumn("addr_id", row_number().over(Window.orderBy(lit(1))))
 
+triple = with_usd.join(addrs_abridged, with_usd.addr == addrs_abridged.addr, 'inner')\
+                 .select(addrs_abridged.addr_id, with_usd.id, 'bal_usd')\
+                 .withColumnRenamed('id', 'tok_id')
 
-# Brainstorming
-# Create list of unique addresses (set union of from_address and to_address)
-# Iterate over this list and compute balances as above
-# Compose the triplet table from these repeated calculations
-#   1) Calculate balance for wallet_addr
-#   2) Add entry to triple_df of (wallet_addr, tok_id, balance)
-#   3) Join triple_df with toks_silver
-#   4) Convert balance to USD
-#   5) Replace tok_id with tok_addr (?) (Is this necessary? We could probably just join the recommendations (token id's) from the model with toks_silver to get the relevant info before presenting it to the user)
+triple.write\
+      .format("delta")\
+      .mode("overwrite")\
+      .option("mergeSchema", False)\
+      .saveAsTable("g04_db.triple_test")
+
+# COMMAND ----------
+
+triple.count()
 
 # COMMAND ----------
 
